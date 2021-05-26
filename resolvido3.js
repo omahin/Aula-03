3975150b3c0987fe17e741291271607db53a6f8e
Olga Mahin <olgamahin@hotmail.com>
2021-05-26 01:44:49 -0300

    Adicionei resposta da questão 3

---
+++ resolvido3.js
@@ -0,0 +1,11 @@
+//3 - Crie uma função que recebe 2 parâmetros e retorna o resultado da divisão entre eles. Além disso, se o resto dessa divisão for zero deverá imprimir no console o valor e dizer que ele é par.
+
+function parOuImpar(numero1,numero2){
+    if(numero1*numero2%2 === 0){
+        return `Este número é par : ${numero1*numero2}`
+    }  
+    else if(numero1*numero2%2 !== 0){
+        return `Este número é ímpar : ${numero1*numero2}`
+    }
+}
+console.log (parOuImpar(9,2))
