871d9d2c0102b9b30e9bb0c83f96d3f7571328e4
Olga Mahin <olgamahin@hotmail.com>
2021-05-26 01:44:23 -0300

    Adicionei resposta da questão 2

---
+++ resolvido2.js
@@ -0,0 +1,6 @@
+//2 - crie um algoritmo que converte dias em horas, quando recebe um número de dias.
+
+function diasEmHoras(numeroDeDias){
+    return numeroDeDias*24
+}
+console.log(diasEmHoras(8))
